88a17b5db506e6b898e27c1a5b5293e065f9b02e
Sebastian Allen <seb@Jays-iMac.local>
2016-07-05 17:52:51 -0700

    hi there

--- cat.py
+++ cat.py
@@ -1,5 +1,11 @@
 import math
-n = int(input("Enter n: "))
-print (math.factorial(n))
-if n <= 0:
-    print ("0 is not a valid #")
+n = int(input("Enter a whole number greater than 0: "))
+b = (math.factorial(n)) + 2
+a = 1
+while 1 <= n:
+    a = a * n
+    n = n - 1
+if a == b:
+    print (a)
+else: 
+    print ("ERROR :(")
